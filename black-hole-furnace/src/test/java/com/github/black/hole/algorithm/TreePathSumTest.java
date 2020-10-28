93e5c3a5466c7f5e47803effd2fd0a5889e3fceb
hairen.lhr <hairen.lhr@alibaba-inc.com>
2020-10-28 09:44:27 +0800

    source

---
+++ black-hole-furnace/src/test/java/com/github/black/hole/algorithm/TreePathSumTest.java
@@ -0,0 +1,17 @@
+package com.github.black.hole.algorithm;
+
+import com.github.black.hole.base.algorithm.tree.TreeNode;
+
+import java.util.List;
+
+/**
+ * @author hairen.long
+ * @date 2020/10/27
+ */
+public class TreePathSumTest {
+
+    public List<List<Integer>> pathSum(TreeNode node, int sum){
+
+        return null;
+    }
+}
